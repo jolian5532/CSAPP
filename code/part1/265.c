8f466e73c463c8b434c1b3696abfb17388748a60
jolian5532 <jolianmohmmad@gmail.com>
2024-01-22 20:06:15 +0100

    smh

--- code/part1/265.c
+++ code/part1/265.c
@@ -30,61 +30,23 @@ operations.
 0101 5
 1010 10
 1111 15
-
+SMH!! was gonna do recursion
+ 
 
 */
 #include <stdio.h>
 
 int odd_ones(unsigned x){
-  /* TODO */
-  int mask = 15;
-  int result = 0;
-  int odd = !(!((x&mask)%3) || !((x&mask)%5)) ;
-  x = x >> 4;
-  result = result + odd;
-  odd = !(!((x&mask)%3) || !((x&mask)%5)) ;
-  x = x >> 4;
-  result = result + odd;
-  odd = !(!((x&mask)%3) || !((x&mask)%5)) ;
-  x = x >> 4;
-  result = result + odd;
-  odd = !(!((x&mask)%3) || !((x&mask)%5)) ;
-  x = x >> 4;
-  result = result + odd;
-  odd = !(!((x&mask)%3) || !((x&mask)%5)) ;
-  x = x >> 4;
-  result = result + odd;
-  odd = !(!((x&mask)%3) || !((x&mask)%5)) ;
-  x = x >> 4;
-  result = result + odd;
-  odd = !(!((x&mask)%3) || !((x&mask)%5)) ;
-  x = x >> 4;
-  result = result + odd;
-  odd = !(!((x&mask)%3) || !((x&mask)%5)) ;
-  x = x >> 4;
-  result = result + odd;
-  printf("%d\n",result);
+  x ^= (x >> 16);
+  x ^= (x >> 8);
+  x ^= (x >> 4);
+  x ^= (x >> 2);
+  x ^= (x >> 1);
+  return x & 1;  
 }
 
 
 int main(){
+  printf("%d\n",odd_ones(0b0100000100000101)); // odd
   
-  odd_ones(1<<4+15);
-  odd_ones(2<<4);
-  odd_ones(4<<4);
-  odd_ones(8<<4);
-  odd_ones(7<<4);
-  odd_ones(14<<4);
-  odd_ones(13<<4);
-  odd_ones(11<<4);
-  printf("none odd\n");
-  odd_ones(3<<4);
-  odd_ones(6<<4);
-  odd_ones(9<<4);
-  odd_ones(12<<4);
-  odd_ones(5<<4);
-  odd_ones(10<<4);
-  odd_ones(15<<4);
-  
-  odd_ones(124);
 }
